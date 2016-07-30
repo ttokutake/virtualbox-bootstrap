36b33874dcb8d9c57d2d57948b2ed6b32cd126e0
ttokutake <tadatoshi.tokutake@gmail.com>
2016-07-30 18:03:49 +0900

    [Feature] Add confirmation whether VBox Guest Additions' disc is inserted

--- debian/main.sh
+++ debian/main.sh
@@ -18,5 +18,11 @@ SCRIPT_DIR=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
 apt update
 apt upgrade -y
 
-bash $SCRIPT_DIR/set_essential.sh            $USER_NAME
+bash $SCRIPT_DIR/set_essential.sh $USER_NAME
+
+input=''
+while [ "$input" != 'ok' ]; do
+  echo -n "Please input \"ok\" after inserting VBox Guest Additions' disc: "
+  read input
+done
 bash $SCRIPT_DIR/set_vbox_guest_additions.sh $USER_NAME
