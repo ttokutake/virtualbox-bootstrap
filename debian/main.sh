375652c6a7842c803cb6e1e0cf0aa8a518fa1bbf
ttokutake <tadatoshi.tokutake@gmail.com>
2016-07-31 16:35:12 +0900

    [Refactoring] Move files to "lib" directory

--- debian/main.sh
+++ debian/main.sh
@@ -12,18 +12,19 @@ if [ "$USER_NAME" == '' ]; then
 fi
 
 SCRIPT_DIR=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
+LIB_DIR=$SCRIPT_DIR/lib
 
 
 apt update
 apt upgrade -y
 
-bash $SCRIPT_DIR/set_essential.sh $USER_NAME
+bash $LIB_DIR/set_essential.sh $USER_NAME
 
 input=''
 while [ "$input" != 'ok' ]; do
   echo -n "Please input \"ok\" after inserting VBox Guest Additions' disc: "
   read input
 done
-bash $SCRIPT_DIR/set_vbox_guest_additions.sh $USER_NAME
+bash $LIB_DIR/set_vbox_guest_additions.sh $USER_NAME
 
 echo 'Please reboot Debian.'
