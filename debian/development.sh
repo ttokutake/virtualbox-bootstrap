c2c3606034667ee27c7a7ec4c4514113a0fc89b2
ttokutake <tadatoshi.tokutake@gmail.com>
2017-09-21 21:13:15 +0900

    Install Docker CE

--- debian/development.sh
+++ debian/development.sh
@@ -90,3 +90,17 @@ echo '### Install Go ###'
 asdf plugin-add golang https://github.com/kennyp/asdf-golang.git
 asdf install    golang 1.8
 echo
+
+
+echo '### Install Docker CE ###'
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common
+curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install -y docker-ce
+echo
+
+echo '### Install Docker Compose ###'
+sudo curl -L https://github.com/docker/compose/releases/download/1.16.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+echo
