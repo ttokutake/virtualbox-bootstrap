09ce852c96217c0a62063c93921c30973876412c
ttokutake <tadatoshi.tokutake@gmail.com>
2018-04-23 23:00:37 +0900

    Add script for installing Docker

--- debian/development.sh
+++ debian/development.sh
@@ -92,17 +92,3 @@ echo
 # sudo apt install autoconf libblocksruntime-dev
 # asdf plugin-add swift
 # asdf install    swift 4.0
-
-
-echo '### Install Docker CE ###'
-sudo apt-get install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common
-curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") $(lsb_release -cs) stable"
-sudo apt-get update
-sudo apt-get install -y docker-ce
-echo
-
-echo '### Install Docker Compose ###'
-sudo curl -L https://github.com/docker/compose/releases/download/1.16.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-echo
---
+++ debian/docker.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -euo pipefail
+
+BASHRC=~/.bashrc
+
+echo '### Install Docker CE ###'
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common
+curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install -y docker-ce
+echo
+
+echo '### Install Docker Compose ###'
+sudo curl -Lo /usr/local/bin/docker-compose https://github.com/docker/compose/releases/download/1.21.0/docker-compose-`uname -s`-`uname -m`
+sudo chmod +x /usr/local/bin/docker-compose
+echo
+
+echo '### Install Kubectl'
+sudo curl -Lo /usr/local/bin/kubectl https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+sudo chmod +x /usr/local/bin/kubectl
+echo
+
+echo '### Install Minikube ###'
+sudo curl -Lo /usr/local/bin/minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo chmod +x /usr/local/bin/minikube
+mkdir $HOME/.kube || true
+touch $HOME/.kube/config
+echo 'export MINIKUBE_WANTUPDATENOTIFICATION=false' >> $BASHRC
+echo 'export MINIKUBE_WANTREPORTERRORPROMPT=false'  >> $BASHRC
+echo 'export MINIKUBE_HOME=$HOME'                   >> $BASHRC
+echo 'export CHANGE_MINIKUBE_NONE_USER=true'        >> $BASHRC
+echo 'export KUBECONFIG=$HOME/.kube/config'         >> $BASHRC
+echo
