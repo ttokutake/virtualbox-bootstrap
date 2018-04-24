ba0c8c70005cdd63c28d464c79657c335054124b
ttokutake <tadatoshi.tokutake@gmail.com>
2018-04-24 22:24:50 +0900

    Cannot use minikube

--- debian/docker.sh
+++ debian/docker.sh
@@ -19,21 +19,21 @@ sudo curl -Lo /usr/local/bin/docker-compose https://github.com/docker/compose/re
 sudo chmod +x /usr/local/bin/docker-compose
 echo
 
-echo '### Install Kubectl'
-sudo curl -Lo /usr/local/bin/kubectl https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
-sudo chmod +x /usr/local/bin/kubectl
-echo
+# echo '### Install Kubectl'
+# sudo curl -Lo /usr/local/bin/kubectl https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+# sudo chmod +x /usr/local/bin/kubectl
+# echo
 
-echo '### Install Minikube ###'
-sudo curl -Lo /usr/local/bin/minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
-sudo chmod +x /usr/local/bin/minikube
-mkdir $HOME/.kube || true
-touch $HOME/.kube/config
-echo 'export MINIKUBE_WANTUPDATENOTIFICATION=false' >> $BASHRC
-echo 'export MINIKUBE_WANTREPORTERRORPROMPT=false'  >> $BASHRC
-echo 'export MINIKUBE_HOME=$HOME'                   >> $BASHRC
-echo 'export CHANGE_MINIKUBE_NONE_USER=true'        >> $BASHRC
-echo 'export KUBECONFIG=$HOME/.kube/config'         >> $BASHRC
-echo
+# echo '### Install Minikube ###'
+# sudo curl -Lo /usr/local/bin/minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+# sudo chmod +x /usr/local/bin/minikube
+# mkdir $HOME/.kube || true
+# touch $HOME/.kube/config
+# echo 'export MINIKUBE_WANTUPDATENOTIFICATION=false' >> $BASHRC
+# echo 'export MINIKUBE_WANTREPORTERRORPROMPT=false'  >> $BASHRC
+# echo 'export MINIKUBE_HOME=$HOME'                   >> $BASHRC
+# echo 'export CHANGE_MINIKUBE_NONE_USER=true'        >> $BASHRC
+# echo 'export KUBECONFIG=$HOME/.kube/config'         >> $BASHRC
+# echo
 
-# sudo -E minikube start --vm-driver=none
+# [Command to start minikube]: sudo -E minikube start --vm-driver=none
