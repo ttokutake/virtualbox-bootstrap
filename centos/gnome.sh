7d47ce5ca04528c812848b55648517dc135a0043
Tadatoshi Tokutake <tadatoshi.tokutake@gmail.com>
2018-06-20 20:59:17 +0900

    Update gnome.sh

--- centos/gnome.sh
+++ centos/gnome.sh
@@ -1,6 +1,6 @@
-USER=`whoami`
-sudo sed -i -E 's/AutomaticLoginEnable=.*/AutomaticLoginEnable=True/' /etc/gdm/custom.conf
-sudo sed -i -E "s/AutomaticLogin=.*/AutomaticLogin=$USER/" /etc/gdm/custom.conf
+# USER=`whoami`
+# sudo sed -i -E 's/AutomaticLoginEnable=.*/AutomaticLoginEnable=True/' /etc/gdm/custom.conf
+# sudo sed -i -E "s/AutomaticLogin=.*/AutomaticLogin=$USER/" /etc/gdm/custom.conf
 
 # Rename directory names
 LANG=C xdg-user-dirs-gtk-update
