40534b0156cf788e0f6c381a7fa83231612a1be9
ttokutake <tadatoshi.tokutake@gmail.com>
2018-06-24 13:03:29 +0900

    Install direnv

--- centos/development.sh
+++ centos/development.sh
@@ -5,7 +5,7 @@ test -d ~/.linuxbrew && PATH="$HOME/.linuxbrew/bin:$HOME/.linuxbrew/sbin:$PATH"
 test -d /home/linuxbrew/.linuxbrew && PATH="/home/linuxbrew/.linuxbrew/bin:/home/linuxbrew/.linuxbrew/sbin:$PATH"
 test -r ~/.bash_profile && echo "export PATH='$(brew --prefix)/bin:$(brew --prefix)/sbin'":'"$PATH"' >>~/.bash_profile
 
-brew install git vim
+brew install git vim direnv
 
 # Install asdf
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.5.0
