62f9c5036ac98aaca568eef53b89d08ca610d517
ttokutake <tadatoshi.tokutake@gmail.com>
2018-07-02 12:19:06 +0900

    Allow to use docker command without sudo

--- centos/development.sh
+++ centos/development.sh
@@ -24,3 +24,4 @@ sudo yum install -y yum-utils device-mapper-persistent-data lvm2
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 sudo yum install docker-ce
 sudo systemctl enable docker
+sudo usermod -aG docker `whoami`
