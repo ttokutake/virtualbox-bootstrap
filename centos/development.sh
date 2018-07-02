ad9c88a46a61c064ad931d7870fdfedd24190c78
ttokutake <tadatoshi.tokutake@gmail.com>
2018-07-02 13:36:35 +0900

    Install Docker Compose

--- centos/development.sh
+++ centos/development.sh
@@ -25,3 +25,5 @@ sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/dock
 sudo yum install docker-ce
 sudo systemctl enable docker
 sudo usermod -aG docker `whoami`
+sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
