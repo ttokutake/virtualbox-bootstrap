c0eddcfa97e033e9ec1b38d97f7f4fd46b6736dd
ttokutake <tadatoshi.tokutake@gmail.com>
2018-10-04 14:02:10 +0900

    Increase max_user_watches

--- centos/centos.sh
+++ centos/centos.sh
@@ -4,5 +4,6 @@ sudo grub2-mkconfig -o /boot/grub2/grub.cfg
 sudo sed -i -E 's/SELINUX=.*/SELINUX=disabled/' /etc/selinux/config
 
 # echo 'vm.max_map_count=262144' >> /etc/sysctl.conf
+# echo 'fs.inotify.max_user_watches=524288' >> /etc/sysctl.conf
 
 sudo systemctl disable firewalld
