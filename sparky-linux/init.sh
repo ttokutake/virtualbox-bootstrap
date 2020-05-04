e59ee1d340620c5cbba1d83f1cf8fdec28cd3af7
ttokutake <tadatoshi.tokutake@gmail.com>
2020-05-04 10:18:22 +0900

    Add docker-compose

--- sparky-linux/init.sh
+++ sparky-linux/init.sh
@@ -40,3 +40,5 @@ echo '. $(brew --prefix asdf)/etc/bash_completion.d/asdf.bash' >> ~/.bash_profil
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 sudo usermod -aG docker ${USER}
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
