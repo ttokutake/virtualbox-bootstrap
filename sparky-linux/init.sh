490149cb9f6ae8e764631292ab6f7e7c909da2ca
Tadatoshi Tokutake <tadatoshi.tokutake@gmail.com>
2020-05-12 09:56:48 +0900

    Update init.sh

--- sparky-linux/init.sh
+++ sparky-linux/init.sh
@@ -42,3 +42,12 @@ sudo sh get-docker.sh
 sudo usermod -aG docker ${USER}
 sudo curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+
+# Install AWS CLI
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+rm -rf awscliv2.zip ./aws
+
+# Remove unused packages
+sudo apt purge qcalculator xfburn hexchat persepolis pidgin thunderbird qbittorrent libreoffice libreoffice-core libreoffice-common qpdfview skanlite cheese qmmp sparky-tube vlc vokoscreen
